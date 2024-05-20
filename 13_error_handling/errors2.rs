16b95bd2ad7aa36a7cd95e92861cf3665ddae37a
elfabri <navarro.fabricio.nahuel@gmail.com>
2024-05-20 14:31:35 -0300

    errors handled

--- 13_error_handling/errors2.rs
+++ 13_error_handling/errors2.rs
@@ -19,16 +19,33 @@
 // Execute `rustlings hint errors2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
+    let qty = item_quantity.parse::<i32>()?;
 
     Ok(qty * cost_per_item + processing_fee)
+
+    /*
+    let qty = match item_quantity.parse::<i32>() {
+        Ok(qty) => qty * cost_per_item + processing_fee,
+        Err(e) => return Err(e)
+    };
+
+    Ok(qty) 
+    */
+
+    /*
+    let qty = item_quantity.parse::<i32>();
+    match qty {
+        Ok(qty) => qty * cost_per_item + processing_fee,
+        Err(error) => return Err(error),
+    };
+
+    Ok(qty? * cost_per_item + processing_fee)
+    */
 }
 
 #[cfg(test)]

debaa6d8d78e917e27c6eec2767645a8f23eccbe
elfabri <navarro.fabricio.nahuel@gmail.com>
2024-05-22 20:28:22 -0300

    tests

--- 17_tests/tests3.rs
+++ 17_tests/tests3.rs
@@ -7,8 +7,6 @@
 // Execute `rustlings hint tests3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 pub fn is_even(num: i32) -> bool {
     num % 2 == 0
 }
@@ -19,11 +17,17 @@ mod tests {
 
     #[test]
     fn is_true_when_even() {
-        assert!();
+        assert!(is_even(2));
+        assert!(is_even(4));
+        assert!(is_even(8));
+        assert!(is_even(28));
     }
 
     #[test]
     fn is_false_when_odd() {
-        assert!();
+        assert!(!is_even(3));
+        assert!(!is_even(9));
+        assert!(!is_even(17));
+        assert!(!is_even(29));
     }
 }

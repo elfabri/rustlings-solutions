02f165e8a5c370735ecf63d69a3eea6f765f9f21
elfabri <navarro.fabricio.nahuel@gmail.com>
2024-08-10 20:57:01 -0300

    clyppy things

---
+++ 22_clippy/target/debug/deps/clippy3-9cc0a0c49cecf8db.d
@@ -0,0 +1,9 @@
+/home/ubu-fab/lab/rustlings/exercises/22_clippy/target/debug/deps/libclippy3-9cc0a0c49cecf8db.rmeta: clippy3.rs Cargo.toml
+
+/home/ubu-fab/lab/rustlings/exercises/22_clippy/target/debug/deps/clippy3-9cc0a0c49cecf8db.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR

a680fcff03c005ba5f2f9fd65999f62de0512440
elfabri <navarro.fabricio.nahuel@gmail.com>
2025-02-17 17:58:45 -0300

    a

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -8,8 +8,12 @@ mod tests {
     fn slice_out_of_array() {
         let a = [1, 2, 3, 4, 5];
 
-        // TODO: Get a slice called `nice_slice` out of the array `a` so that the test passes.
+        // Get a slice called `nice_slice` out of the array `a` so that the test passes.
         // let nice_slice = ???
+        let nice_slice = &a[1..4];  // inclusive .. not inclusive indexes
+
+        // indicating the actual index of the last element to consider
+        // let nice_slice = &a[1..=3];
 
         assert_eq!([2, 3, 4], nice_slice);
     }
